8603c03b2fb5394e6ee817c27b6a6a674609e5ce
brandon <brandon@brandon-iMac.local>
2018-08-14 15:40:22 +0800

    提交

---
+++ venv/demo/demo-datatype.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+#__author__'brandon';
+a = 100    # 整数类型
+b = 100.1   # 浮点类型
+name = "张三" # 字符类型
+
+# print (a+"==,"+b+"==,"+name)
+# print (name)
+
+a = b = c = 1 # 多个变量同时赋值
+# print(a+b+c)
+
+a , b , c , d = 1 , 2.5 , "niHao" ,4+3j
+# print(type(a),type(b),type(c),type(d))
+
+a = 1
+# print(isinstance(a,int))
+
+
+# class A:
+#     pass
+#
+# class B(A):
+#     pass
+#
+# isinstance(A(), A)  # returns True
+# type(A()) == A      # returns True
+# isinstance(B(), A)    # returns True
+# type(B()) == A        # returns False
+
+# var1 = 1
+# var2 = 10
+# print(var1)
+# print(var2)
+# del var2
+# print(var1)
+# print(var2)
+
